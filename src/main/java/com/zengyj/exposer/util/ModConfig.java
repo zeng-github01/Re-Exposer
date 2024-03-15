644cd7815e14455e0c1076530651af2ba3bb13c3
Zengyj <zengyj11111@gmail.com>
2024-03-16 00:08:23 +0800

    Fix Config

--- src/main/java/com/zengyj/exposer/util/ModConfig.java
+++ src/main/java/com/zengyj/exposer/util/ModConfig.java
@@ -8,8 +8,10 @@ public class ModConfig {
 
     @Config.Name("Grid Expose")
     @Config.Comment({"Should the Grid expose the networks contents? (requires Enable Exposing to be true)"})
+    @Config.RequiresWorldRestart
     public static boolean gridExpose = true;
     @Config.Name("Enable Exposing")
     @Config.Comment({"Enables the Controller to expose the network."})
+    @Config.RequiresWorldRestart
     public static boolean expose = true;
 }
