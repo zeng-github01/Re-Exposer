3f31e2643e69f57e2dd70c2ef12e7f57f06850da
zeng-git <95841646+zeng-github01@users.noreply.github.com>
2024-06-06 02:03:51 +0800

    Add Creative Tab

--- src/main/java/com/zengyj/exposer/Registry.java
+++ src/main/java/com/zengyj/exposer/Registry.java
@@ -1,9 +1,8 @@
 package com.zengyj.exposer;
 
-
 import com.zengyj.exposer.block.BlockExposer;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -15,12 +14,22 @@ import net.minecraftforge.registries.RegistryObject;
 public class Registry {
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Constants.Mod_ID);
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Constants.Mod_ID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Constants.Mod_ID);
 
     public static final RegistryObject<Block> EXPOSER = BLOCKS.register("exposer", BlockExposer::new);
     public static final RegistryObject<Item> EXPOSER_ITEM = ITEMS.register("exposer", () -> new BlockItem(EXPOSER.get(),new Item.Properties()));
 
+    public static final RegistryObject<CreativeModeTab> TAB = CREATIVE_TABS.register("exposer", ()->
+            CreativeModeTab.builder().withTabsBefore(CreativeModeTabs.SPAWN_EGGS)
+                    .icon(()-> ItemStack.EMPTY).displayItems((itemDisplayParameters, output) -> {
+                        output.accept(EXPOSER_ITEM.get());
+                    }).build());
+
     public Registry() {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        CREATIVE_TABS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
+
+
 }
