aa7901d70385e3e2557f152edbfc441a37e3be7f
zeng-git <95841646+zeng-github01@users.noreply.github.com>
2024-06-06 11:10:07 +0800

    Add translation

--- src/main/java/com/zengyj/exposer/node/NetworkNodeExposer.java
+++ src/main/java/com/zengyj/exposer/node/NetworkNodeExposer.java
@@ -37,7 +37,6 @@ public class NetworkNodeExposer extends NetworkNode {
         this.fluidHandler = new FluidHandler(network);
         network.getItemStorageCache().addListener(this.itemHandler);
         network.getFluidStorageCache().addListener(this.fluidHandler);
-        markDirty();
     }
 
     @Override
@@ -45,7 +44,6 @@ public class NetworkNodeExposer extends NetworkNode {
         super.onDisconnected(network);
         network.getItemStorageCache().removeListener(this.itemHandler);
         network.getFluidStorageCache().removeListener(this.fluidHandler);
-        markDirty();
     }
 
     public IItemHandler getItemHandler() {
